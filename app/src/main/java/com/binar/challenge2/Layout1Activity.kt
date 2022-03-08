9b841d4f46db2259236a7fab684a7bcb19e232f5
firmanfa <fafirmansyah20@gmail.com>
2022-03-08 18:45:46 +0700

    feat: setup gojek toolbar and bottomnavbar

--- app/src/main/java/com/binar/challenge2/Layout1Activity.kt
+++ app/src/main/java/com/binar/challenge2/Layout1Activity.kt
@@ -2,10 +2,22 @@ package com.binar.challenge2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.binar.challenge2.databinding.ActivityLayout1Binding
 
 class Layout1Activity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLayout1Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_layout1)
+        binding = ActivityLayout1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.gojek_toolbar_menu, menu)
+        return true
     }
 }
--- app/src/main/java/com/binar/challenge2/MainActivity.kt
+++ app/src/main/java/com/binar/challenge2/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.binar.challenge2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.binar.challenge2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLayout1.setOnClickListener {
+            val intent = Intent(this, Layout1Activity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnLayout2.setOnClickListener {
+            val intent = Intent(this, Layout2Activity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
