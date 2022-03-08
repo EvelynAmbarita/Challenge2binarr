6e5c910ec345a34f425ba42fdc5003e85d6e3fca
firmanfa <fafirmansyah20@gmail.com>
2022-03-08 15:32:39 +0700

    Initial commit

---
+++ app/src/main/java/com/binar/challenge2/Layout1Activity.kt
@@ -0,0 +1,11 @@
+package com.binar.challenge2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Layout1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout1)
+    }
+}
---
+++ app/src/main/java/com/binar/challenge2/Layout2Activity.kt
@@ -0,0 +1,11 @@
+package com.binar.challenge2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Layout2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout2)
+    }
+}
