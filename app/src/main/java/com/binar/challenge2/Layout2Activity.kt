7f3f18288dc8f76be7bf746cdc9a9c0217f255ad
firmanfa <fafirmansyah20@gmail.com>
2022-03-10 18:58:00 +0700

    feat: done all

--- app/src/main/java/com/binar/challenge2/Layout2Activity.kt
+++ app/src/main/java/com/binar/challenge2/Layout2Activity.kt
@@ -1,11 +1,12 @@
 package com.binar.challenge2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.util.Log
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import com.binar.challenge2.databinding.ActivityLayout2Binding
-import com.binar.challenge2.tokped.MenuAdapter
 
 class Layout2Activity : AppCompatActivity() {
 
@@ -20,6 +21,42 @@ class Layout2Activity : AppCompatActivity() {
         binding.barangLayout2.progressBar2.isEnabled = false
         binding.barangLayout3.progressBar2.isEnabled = false
         binding.barangLayout4.progressBar2.isEnabled = false
+
+        binding.nestedScrollView.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            Log.i("scroll",scrollY.toString())
+            val headerHeight = binding.header.height
+            if (scrollY>headerHeight){
+                binding.include.clToolbar.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+                val window = this.window
+//                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+//                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+//                window.statusBarColor = ContextCompat.getColor(this, R.color.transparent)
+                binding.include.svSearch.background =
+                    ResourcesCompat.getDrawable(resources, R.drawable.search_bar_bordered,theme)
+                binding.include.imageView.setColorFilter(ContextCompat.getColor(this,R.color.black), android.graphics.PorterDuff.Mode.SRC_IN)
+                binding.include.imageView12.setColorFilter(ContextCompat.getColor(this,R.color.black), android.graphics.PorterDuff.Mode.SRC_IN)
+                binding.include.imageView13.setColorFilter(ContextCompat.getColor(this,R.color.black), android.graphics.PorterDuff.Mode.SRC_IN)
+                binding.include.imageView14.setColorFilter(ContextCompat.getColor(this,R.color.black), android.graphics.PorterDuff.Mode.SRC_IN)
+            }else{
+                binding.include.clToolbar.setBackgroundColor(ContextCompat.getColor(this, R.color.tokped_primary))
+                val window = this.window
+
+//                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+//                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+//                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+                window.statusBarColor = ContextCompat.getColor(this, R.color.tokped_primary)
+
+                binding.include.clToolbar.elevation = 2f
+                binding.include.svSearch.background =
+                    ResourcesCompat.getDrawable(resources, R.drawable.search_bar_bordered,theme)
+                binding.include.imageView.setColorFilter(ContextCompat.getColor(this,R.color.white), android.graphics.PorterDuff.Mode.SRC_IN)
+                binding.include.imageView12.setColorFilter(ContextCompat.getColor(this,R.color.white), android.graphics.PorterDuff.Mode.SRC_IN)
+                binding.include.imageView13.setColorFilter(ContextCompat.getColor(this,R.color.white), android.graphics.PorterDuff.Mode.SRC_IN)
+                binding.include.imageView14.setColorFilter(ContextCompat.getColor(this,R.color.white), android.graphics.PorterDuff.Mode.SRC_IN)
+
+            }
+        }
 
 
 
