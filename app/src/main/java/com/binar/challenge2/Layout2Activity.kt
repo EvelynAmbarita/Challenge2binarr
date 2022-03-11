418138ac65276c55b14e22063efbce0938d59dd4
firmanfa <fafirmansyah20@gmail.com>
2022-03-11 19:03:12 +0700

    feat: add scroll transition on toolbar

--- app/src/main/java/com/binar/challenge2/Layout2Activity.kt
+++ app/src/main/java/com/binar/challenge2/Layout2Activity.kt
@@ -1,12 +1,15 @@
 package com.binar.challenge2
 
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
 import android.os.Bundle
-import android.util.Log
-import android.view.WindowManager
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.binar.challenge2.databinding.ActivityLayout2Binding
+import com.bumptech.glide.Glide
 
 class Layout2Activity : AppCompatActivity() {
 
@@ -22,59 +25,102 @@ class Layout2Activity : AppCompatActivity() {
         binding.barangLayout3.progressBar2.isEnabled = false
         binding.barangLayout4.progressBar2.isEnabled = false
 
+        Glide.with(this).load(R.drawable.produk_1).fitCenter().into(binding.barangLayout1.imageView19)
+        Glide.with(this).load(R.drawable.produk_2).fitCenter().into(binding.barangLayout2.imageView19)
+        Glide.with(this).load(R.drawable.produk_3).fitCenter().into(binding.barangLayout3.imageView19)
+        Glide.with(this).load(R.drawable.produk_4).fitCenter().into(binding.barangLayout4.imageView19)
+
+        Glide.with(this).load(R.drawable.video_1).fitCenter().into(binding.videoLayout1.imageView19)
+        Glide.with(this).load(R.drawable.video_2).fitCenter().into(binding.videoLayout2.imageView19)
+        Glide.with(this).load(R.drawable.video_3).fitCenter().into(binding.videoLayout3.imageView19)
+        Glide.with(this).load(R.drawable.video_4).fitCenter().into(binding.videoLayout4.imageView19)
+
+        object : CountDownTimer(600000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                binding.tvWaktu.text = "${millisUntilFinished/1000/60} menit ${millisUntilFinished/1000%60} detik"
+//                Log.i("timer","${millisUntilFinished/1000/60} menit ${millisUntilFinished/1000%60} detik")
+            }
+
+            override fun onFinish() {
+                binding.tvWaktu.text = "promo selesai"
+            }
+        }.start()
+
+        var isAlreadyAnimateWhite = false
+        var isAlreadyAnimateGreen = true
+
         binding.nestedScrollView.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
-            Log.i("scroll",scrollY.toString())
+
             val headerHeight = binding.header.height
-            if (scrollY>headerHeight){
-                binding.include.clToolbar.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
-                val window = this.window
-//                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-//                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
-//                window.statusBarColor = ContextCompat.getColor(this, R.color.transparent)
-                binding.include.svSearch.background =
-                    ResourcesCompat.getDrawable(resources, R.drawable.search_bar_bordered,theme)
-                binding.include.imageView.setColorFilter(ContextCompat.getColor(this,R.color.black), android.graphics.PorterDuff.Mode.SRC_IN)
-                binding.include.imageView12.setColorFilter(ContextCompat.getColor(this,R.color.black), android.graphics.PorterDuff.Mode.SRC_IN)
-                binding.include.imageView13.setColorFilter(ContextCompat.getColor(this,R.color.black), android.graphics.PorterDuff.Mode.SRC_IN)
-                binding.include.imageView14.setColorFilter(ContextCompat.getColor(this,R.color.black), android.graphics.PorterDuff.Mode.SRC_IN)
-            }else{
-                binding.include.clToolbar.setBackgroundColor(ContextCompat.getColor(this, R.color.tokped_primary))
-                val window = this.window
+            when {
+                scrollY>headerHeight -> {
+                    changeToolbarStyle(false)
+                    val window = this.window
+                    val greenTokped = ContextCompat.getColor(this, R.color.tokped_primary)
+                    val white = ContextCompat.getColor(this, R.color.white)
+                    WindowInsetsControllerCompat(window,window.decorView).isAppearanceLightStatusBars = true
+                    if (!isAlreadyAnimateWhite){
+                        val colorAnimate = ValueAnimator.ofObject(ArgbEvaluator(),greenTokped,white)
+                        colorAnimate.addUpdateListener {
+                            window.statusBarColor = it.animatedValue as Int
+                            binding.include.clToolbar.setBackgroundColor(it.animatedValue as Int)
+                        }
+                        colorAnimate.duration = 250
+                        colorAnimate.start()
+                        isAlreadyAnimateWhite = true
+                        isAlreadyAnimateGreen = false
+                    }
+                }
 
-//                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-//                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-//                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
-                window.statusBarColor = ContextCompat.getColor(this, R.color.tokped_primary)
+                else -> {
 
-                binding.include.clToolbar.elevation = 2f
-                binding.include.svSearch.background =
-                    ResourcesCompat.getDrawable(resources, R.drawable.search_bar_bordered,theme)
-                binding.include.imageView.setColorFilter(ContextCompat.getColor(this,R.color.white), android.graphics.PorterDuff.Mode.SRC_IN)
-                binding.include.imageView12.setColorFilter(ContextCompat.getColor(this,R.color.white), android.graphics.PorterDuff.Mode.SRC_IN)
-                binding.include.imageView13.setColorFilter(ContextCompat.getColor(this,R.color.white), android.graphics.PorterDuff.Mode.SRC_IN)
-                binding.include.imageView14.setColorFilter(ContextCompat.getColor(this,R.color.white), android.graphics.PorterDuff.Mode.SRC_IN)
+                    changeToolbarStyle(true)
+                    val window = this.window
+                    val greenTokped = ContextCompat.getColor(this, R.color.tokped_primary)
+                    val white = ContextCompat.getColor(this, R.color.white)
+                    WindowInsetsControllerCompat(window,window.decorView).isAppearanceLightStatusBars = false
 
+                    if (!isAlreadyAnimateGreen){
+                        val colorAnimate = ValueAnimator.ofObject(ArgbEvaluator(),white,greenTokped)
+                        colorAnimate.addUpdateListener {
+                            window.statusBarColor = it.animatedValue as Int
+                            binding.include.clToolbar.setBackgroundColor(it.animatedValue as Int)
+                        }
+                        colorAnimate.duration = 250
+                        colorAnimate.start()
+                        isAlreadyAnimateWhite = false
+                        isAlreadyAnimateGreen = true
+                    }
+                }
             }
         }
 
 
 
-//        val listTitle = resources.getStringArray(R.array.menu_tokped_list_title)
-//        val listIcon = intArrayOf(R.drawable.ic_official_selected,
-//            R.drawable.ic_lainnya,
-//            R.drawable.ic_picnic_basket_picnic_svgrepo_com,
-//            R.drawable.ic_electronics_svgrepo_com,
-//            R.drawable.ic_scroll_svgrepo_com,
-//            R.drawable.ic_stationery_svgrepo_com,
-//            R.drawable.ic_money_svgrepo_com,
-//            R.drawable.ic_plane_svgrepo_com,
-//            R.drawable.ic_party_hat_birthday_svgrepo_com)
-//
-//        val menuAdapter = MenuAdapter(this,listTitle, listIcon)
-//        val layoutManager = LinearLayoutManager(this)
-//        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
-//        binding.rvMenu.adapter = menuAdapter
+
+
+
+    }
+
+    private fun changeToolbarStyle(status: Boolean){
+
+        if(status){
+            binding.include.clToolbar.elevation = 2f
+            binding.include.svSearch.background =
+                ResourcesCompat.getDrawable(resources, R.drawable.search_bar_bordered,theme)
+            binding.include.imageView.setColorFilter(ContextCompat.getColor(this,R.color.white), android.graphics.PorterDuff.Mode.SRC_IN)
+            binding.include.imageView12.setColorFilter(ContextCompat.getColor(this,R.color.white), android.graphics.PorterDuff.Mode.SRC_IN)
+            binding.include.imageView13.setColorFilter(ContextCompat.getColor(this,R.color.white), android.graphics.PorterDuff.Mode.SRC_IN)
+            binding.include.imageView14.setColorFilter(ContextCompat.getColor(this,R.color.white), android.graphics.PorterDuff.Mode.SRC_IN)
+        }else{
+            binding.include.svSearch.background =
+                ResourcesCompat.getDrawable(resources, R.drawable.search_bar_bordered,theme)
+            binding.include.imageView.setColorFilter(ContextCompat.getColor(this,R.color.gojek_text_active), android.graphics.PorterDuff.Mode.SRC_IN)
+            binding.include.imageView12.setColorFilter(ContextCompat.getColor(this,R.color.gojek_text_active), android.graphics.PorterDuff.Mode.SRC_IN)
+            binding.include.imageView13.setColorFilter(ContextCompat.getColor(this,R.color.gojek_text_active), android.graphics.PorterDuff.Mode.SRC_IN)
+            binding.include.imageView14.setColorFilter(ContextCompat.getColor(this,R.color.gojek_text_active), android.graphics.PorterDuff.Mode.SRC_IN)
+        }
 
     }
 }
